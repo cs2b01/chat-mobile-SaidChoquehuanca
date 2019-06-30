fcb1b75f2be0908785dcd44c8208a81106a9684b
SaidChoquehuanca <39719094+SaidChoquehuanca@users.noreply.github.com>
2019-06-29 20:36:32 -0500

    Login Activity

---
+++ app/src/main/java/cs2901/utec/chat_mobile/contactsActivity.java
@@ -0,0 +1,14 @@
+package cs2901.utec.chat_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class contactsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacs);
+
+    }
+
+}
